02f8c8f4316feaaab7f72fed37f6cd8f5bcdd5bd
Benedictus79 <benedictus79@disroot.org>
2024-07-14 05:08:55 -0300

    feat: Adicionado multithread e download para conteudo em texto

--- login.py
+++ login.py
@@ -1,6 +1,5 @@
 import requests
-
-from utils import alexandria_ascii_art, clear_screen
+from utils import os, re, alexandria_ascii_art, clear_screen
 
 
 kiwifysession = requests.Session()
@@ -42,30 +41,49 @@ def get_token(username, password):
   return response['idToken']
 
 
+def extract_course_info(course, pattern):
+  for key, value in course.items():
+    if pattern.match(key):
+      return key, value['id'], value['name']
+  return None, None, None
+
+
+def extract_school_id(school):
+  school_id = kiwifysession.get(f'https://admin-api.kiwify.com.br/v1/viewer/schools/{school}/courses').json()['my_courses'][0]['id']
+  return school_id
+
+
 def get_courses(token):
   kiwifysession.headers['authorization'] = f'Bearer {token}'
   response = kiwifysession.get('https://admin-api.kiwify.com.br/v1/viewer/schools/courses?&archived=false', headers=headers).json()
   courses = response['courses']
   course_data = {}
+  pattern = re.compile(r'^(course_info|school_info)$')
+
   for course in courses:
-    course_id = course['course_info']['id']
-    course_name = course['course_info']['name']
-    course_data[course_id] = course_name
+    info_type, course_id, course_name = extract_course_info(course, pattern)
+    if course_id and course_name:
+      if info_type == 'school_info':
+        school_id = extract_school_id(course_id)
+        course_data[school_id] = course_name
+      else:
+        course_data[course_id] = course_name
+
   return course_data
 
 
 def choose_course(courses):
-  print('Cursos disponíveis:')
+  print('Courses:')
   for i, course_name in enumerate(courses.values(), start=1):
     print(f'{i}. {course_name}')
-  choice = input("Escolha um curso pelo número: ")
+  choice = input("Choose a course by number: ")
   selected_course_id = list(courses.keys())[int(choice) - 1]
   selected_course_title = courses[selected_course_id]
   selected_course_link = f'https://admin-api.kiwify.com.br/v1/viewer/courses/{selected_course_id}'
-  print(f'Link curso selecionado:', selected_course_title)
-  print(f'O tamanho da pasta pode ocasionar erros devido a diretórios muito longos.')
-  print(f'É opcional. Caso não coloque nada ou pasta inexistente, o download será feito na pasta desta tools.')
-  selected_course_folder = input(f'Escolha a pasta para download: ').strip()
+  print(f'Selected course link:', f'https://dashboard.kiwify.com.br/course/{selected_course_id}')
+  print(f'The folder size may cause errors due to excessively long directories.')
+  print(f'If you do not specify anything or if the folder does not exist, the download will be done in the folder: {os.getcwd()}.')
+  selected_course_folder = input(f'Choose the folder for download: ').strip()
   return selected_course_title, selected_course_link, selected_course_folder
 
 username, password = credentials()
